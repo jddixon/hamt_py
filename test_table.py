f4ffb352048b1ecbeea8c87a955ce021792473c6
Jim Dixon <jddixon@gmail.com>
2017-03-14 17:16:03 -0700

    add test_with_matching_keys

--- test_table.py
+++ test_table.py
@@ -77,9 +77,73 @@ class TestTable(unittest.TestCase):
             leaves.append(leaf)
         return leaves
 
+    def make_many_leaves(self, count):
+        """
+        Make count Leafs with distinct keys.
+        """
+        if count < 1:
+            count = 16
+        else:
+            count &= 0xffffffff        # arbitrary limit
+        leaves = []
+        keys = set()
+
+        for _ in range(count):
+            key = bytes(self.rng.some_bytes(8))
+            while key in keys:
+                key = bytes(self.rng.some_bytes(8))
+            # we have a unique key; add it to the set
+            keys.add(key)
+
+            # generate a quasi-random value
+            val = bytes(self.rng.some_bytes(16))
+            leaf = Leaf(key, val)
+            leaves.append(leaf)
+        return leaves
+
+    def make_matching_leaves(self, texp):
+        """
+        Make Leafs whose hcodes the same low-order texp x 4 bits.
+
+        This assumes texp == wexp.
+        """
+        keylen = ((4 * texp) + 7) // 8        # that many bytes
+        # DEBUG
+        #print("keylen is %d bytes" % keylen)
+        # END
+
+        suffix_len = 4 * texp   # that many bits
+        mask = (1 << suffix_len) - 1
+
+        key0 = bytes(self.rng.some_bytes(keylen))
+        keys = [key0]
+        hcode0 = uhash(key0)
+        low_bits0 = hcode0 & mask
+        leaves = []
+
+        # DEBUG
+        #print("low order bits: 0x%x" % low_bits0)
+        # END
+        for _ in range(4):          # we want three or more matching keys
+            key = bytes(self.rng.some_bytes(8))
+            low_bits = mask & uhash(key)
+            while low_bits != low_bits0:
+                key = bytes(self.rng.some_bytes(8))
+                low_bits = mask & uhash(key)
+            # DEBUG
+            #print("low order bits: 0x%x" % low_bits0)
+            # END
+            val = bytes(self.rng.some_bytes(16))
+            leaf = Leaf(key, val)
+            leaves.append(leaf)
+        # DEBUG
+        #print("generated %d Leafs" % len(leaves))
+        # END
+        return leaves
+
     # ---------------------------------------------------------------
 
-    def do_test_full_root(self, texp):
+    def do_test_fullish_root(self, texp):
         """
         Test behavior of tree with (nearly) full root with 1 << texp entries.
 
@@ -117,8 +181,8 @@ class TestTable(unittest.TestCase):
         self.assertEqual(len(by_keys), inserted)
 
         # DEBUG
-        print("There are %d/%d entries in the root table." % (
-            len(slot_nbrs), slot_count))
+        # print("There are %d/%d entries in the root table." % (
+        #    len(slot_nbrs), slot_count))
         # END
 
         leaf = self.make_a_unique_leaf(by_keys)
@@ -129,13 +193,82 @@ class TestTable(unittest.TestCase):
         entry = root.find_leaf(leaf.key)
         self.assertIsNotNone(entry)
 
-    def test_full_root(self):
+    def test_fullish_root(self):
         """
-        Test behavior with full roots of various sizes.
+        Test behavior with fullish roots of various sizes.
+
+        We partially fill the root and then add one more leaf, which
+        more often than not splits a slot in the Root table.  Sometimes
+        it splits a slot in the child Table.
 
         """
+        for _ in range(64):
+            for texp in [3, 4, 5, 6]:
+                self.do_test_fullish_root(texp)
+
+    # ---------------------------------------------------------------
+
+    def do_test_with_many_keys(self, texp):
+        """
+        Test behavior of tree where we insert many more Leafs than are
+        necessary to fill the root.
+        """
+
+        wexp = texp     # we aren't yet interested in wexp != texp
+        slot_count = 1 << texp
+        root = Root(wexp, texp)
+        self.assertEqual(root.leaf_count, 0)
+        self.assertEqual(root.table_count, 1)     # root table is counted
+
+        leaves = self.make_many_leaves(slot_count * 2)
+        inserted = 0
+        for leaf in leaves:
+            root.insert_leaf(leaf)
+            inserted += 1
+            self.assertEqual(root.leaf_count, inserted)
+
+        for leaf in leaves:
+            value = root.find_leaf(leaf.key)
+            self.assertEqual(value, leaf.value)
+
+        # we have successfully inserted that many leaf nodes into the tree
+        self.assertEqual(root.leaf_count, inserted)
+
+    def test_with_many_keys(self):
+        """
+        Test behavior with various values of texp and many more Leafs
+        than needed to fill the Root table.
+        """
         for texp in [3, 4, 5, 6]:
-            self.do_test_full_root(texp)
+            self.do_test_with_many_keys(texp)
+
+    # ---------------------------------------------------------------
+
+    def do_test_with_matching_keys(self, texp):
+
+        wexp = texp
+        root = Root(wexp, texp)
+        self.assertEqual(root.leaf_count, 0)
+        self.assertEqual(root.table_count, 1)     # root table is counted
+
+        leaves = self.make_matching_leaves(texp)
+        inserted = 0
+        for leaf in leaves:
+            root.insert_leaf(leaf)
+            inserted += 1
+            self.assertEqual(root.leaf_count, inserted)
+
+        for leaf in leaves:
+            value = root.find_leaf(leaf.key)
+            self.assertEqual(value, leaf.value)
+
+        # we have successfully inserted that many leaf nodes into the tree
+        self.assertEqual(root.leaf_count, inserted)
+
+    def test_with_matching_keys(self):
+
+        for texp in [4]:
+            self.do_test_with_matching_keys(texp)
 
 
 if __name__ == '__main__':
