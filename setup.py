19579cc7330b31c8727d46f9c84ec990eeed63cc
Jim Dixon <jddixon@gmail.com>
2017-07-16 06:05:16 -0700

    add MANIFEST.in, rewrite setup.py; edit cfg files

--- setup.py
+++ setup.py
@@ -1,34 +1,45 @@
 #!/usr/bin/python3
-# ~/dev/py/hamt_py/setup.py
+# hamt_py/setup.py
 
-""" Set up distutils for hamt_py. """
+""" Setuptools project configuration for hamt_py. """
 
 import re
-from distutils.core import setup
+from glob import glob
+from os.path import basename, dirname, exists, join, splitext
+from setuptools import find_packages, setup
+
+# replace with literal
 __version__ = re.search(r"__version__\s*=\s*'(.*)'",
                         open('src/hamt/__init__.py').read()).group(1)
 
-# see http://docs.python.org/distutils/setupscript.html
+# see
+# setuptools.readthedocs.io/en/latest/setuptools.html#new-and-changed-setup-keywords
+
+long_desc = None
+if exists('README.md'):
+    with open('README.md', 'r') as file:
+        long_desc = file.read()
 
 setup(name='hamt_py',
       version=__version__,
       author='Jim Dixon',
       author_email='jddixon@gmail.com',
-      #
-      # wherever we have a .py file that will be imported, we
-      # list it here, without the .py extension but SQuoted
-      py_modules=[],
-      #
-      packages=['src/hamt', ],
-      #
-      # following could be in scripts/ subdir; SQuote
+
+      long_description=long_desc,
+      # packages=find_packages('src'),
+      packages=['hamt'],                                   # LITERAL
+      package_dir={'': 'src'},
+      py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+      include_package_data=False,
+      zip_safe=False,
+
       scripts=[],
-      description='nodeid layer for xlattice_py',
-      url='https://jddixon/github.io/hamt_py',
+      description='generator for hamt_py projects',
+      url='https://jddixon.github.com/hamt_py',
       classifiers=[
-          'Development Status :: 2 - Pre-Alpha',
+          'Development Status :: 2 - Pre-Alpha',                # VARIES
           'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
+          'License :: OSI Approved :: MIT License',             # VARIES
           'Natural Language :: English',
           'Programming Language :: Python 3',
           'Topic :: Software Development :: Libraries :: Python Modules',
