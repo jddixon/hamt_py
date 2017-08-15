843173534d595f6963568c35b10930c2914a6f3f
Jim Dixon <jddixon@gmail.com>
2017-08-15 13:06:18 -0700

    add tox.ini, *requirements.txt; edit cfg files

--- setup.py
+++ setup.py
@@ -3,17 +3,8 @@
 
 """ Setuptools project configuration for hamt_py. """
 
-import re
-from glob import glob
-from os.path import basename, dirname, exists, join, splitext
-from setuptools import find_packages, setup
-
-# replace with literal
-__version__ = re.search(r"__version__\s*=\s*'(.*)'",
-                        open('src/hamt/__init__.py').read()).group(1)
-
-# see
-# setuptools.readthedocs.io/en/latest/setuptools.html#new-and-changed-setup-keywords
+from os.path import exists
+from setuptools import setup
 
 long_desc = None
 if exists('README.md'):
@@ -21,26 +12,28 @@ if exists('README.md'):
         long_desc = file.read()
 
 setup(name='hamt_py',
-      version=__version__,
+      version='0.1.12',
       author='Jim Dixon',
       author_email='jddixon@gmail.com',
-
       long_description=long_desc,
-      # packages=find_packages('src'),
-      packages=['hamt'],                                   # LITERAL
+      packages=['hamt'],
       package_dir={'': 'src'},
-      py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+      py_modules=[],
       include_package_data=False,
       zip_safe=False,
-
       scripts=[],
       description='generator for hamt_py projects',
-      url='https://jddixon.github.com/hamt_py',
+      url='https://jddixon.github.io/hamt_py',
       classifiers=[
-          'Development Status :: 2 - Pre-Alpha',                # VARIES
+          'Development Status :: 2 - Pre-Alpha',
           'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',             # VARIES
+          'License :: OSI Approved :: MIT License',
           'Natural Language :: English',
-          'Programming Language :: Python 3',
+          'Programming Language :: Python 2.7',
+          'Programming Language :: Python 3.3',
+          'Programming Language :: Python 3.4',
+          'Programming Language :: Python 3.5',
+          'Programming Language :: Python 3.6',
+          'Programming Language :: Python 3.7',
           'Topic :: Software Development :: Libraries :: Python Modules',
       ],)
