826f25849044202f70c8f11395276378e88b1a10
Jim Dixon <jddixon@gmail.com>
2018-03-24 13:23:58 -0700

    fix corrupt .dvcz/builds

--- setup.py
+++ setup.py
@@ -6,22 +6,23 @@
 from os.path import exists
 from setuptools import setup
 
-LONG_DESC = None
+long_desc = None
 if exists('README.md'):
     with open('README.md', 'r') as file:
-        LONG_DESC = file.read()
+        long_desc = file.read()
 
 setup(name='hamt_py',
-      version='0.1.16',
+      version='0.1.17',
       author='Jim Dixon',
       author_email='jddixon@gmail.com',
-      long_description=LONG_DESC,
+      long_description=long_desc,
       packages=['hamt'],
       package_dir={'': 'src'},
       py_modules=[],
       include_package_data=False,
       zip_safe=False,
       scripts=[],
+      ext_modules=[],
       description='generator for hamt_py projects',
       url='https://jddixon.github.io/hamt_py',
       classifiers=[
@@ -29,9 +30,10 @@ setup(name='hamt_py',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
           'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python 2',
           'Programming Language :: Python 2.7',
-          'Programming Language :: Python 3.3',
-          'Programming Language :: Python 3.4',
+          'Programming Language :: Python 3',
           'Programming Language :: Python 3.5',
           'Programming Language :: Python 3.6',
           'Programming Language :: Python 3.7',
